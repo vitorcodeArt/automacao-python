829f1d230f9dc5d4c7357fdc3d99f20469bf195c
Vitor Menezes <v.m.moraes1997@gmail.com>
2023-04-14 18:32:14 -0300

    Finalizando projeto

--- robo.py
+++ robo.py
@@ -30,7 +30,7 @@ def enviar_mensagem(numero, texto):
         try:
             enviarMsg = navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span')
             enviarMsg.send_keys(Keys.ENTER)
-            time.sleep(3)
+            time.sleep(6)
             break
         except NoSuchElementException:
             pass
@@ -42,7 +42,22 @@ def enviar_mensagem(numero, texto):
                 navegador.find_element(By.XPATH, '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[2]/div/button').click()
                 break
         except NoSuchElementException:
-            pass        
+            pass    
+        
+def abrir_arquivo():
+    global contatos_df
+    arquivo = filedialog.askopenfilename(filetypes=[("Arquivo Excel", "*.xlsx")])
+    if arquivo:
+        contatos_df = pd.read_excel(arquivo)
+        contatos_df['Mensagem'] = ''
+        contatos_df["Telefone"] = contatos_df.apply(lambda row: ';'.join(str(num).split('.')[0] for num in [row['Telefone 1'], row['Telefone 2'], row['Telefone 3']] if pd.notnull(num)), axis=1)
+        while 'Telefone' not in contatos_df.columns:
+            print('Aguardando criação da coluna "Telefone"...')
+            time.sleep(1)  # aguarda 1 segundo
+        time.sleep(4)
+            
+        nome_arquivo = arquivo.split("/")[-1]  # obtém o nome do arquivo a partir do caminho completo
+        rotulo_arquivo.configure(text=f"Arquivo selecionado: {nome_arquivo}")          
 
 def enviar():
     data = entrada_data.get()
@@ -51,26 +66,31 @@ def enviar():
         endereco = contatos_df.loc[i, "Endereço"]
         numero = contatos_df.loc[i, "Número"]
         complemento = str(contatos_df.loc[i, "Complemento"])
-        ocorrencia = contatos_df.loc[i, "ULTIMO MOTIVO/OCORRENCIA"]
+        ocorrencia = contatos_df.loc[i, "Ultimo Motivo/Ocorrencia"]
         if complemento == 'nan':
             complemento = ''
         os = contatos_df.loc[i, "BA"]
         telefones = str(contatos_df.loc[i, "Telefone"]).split(";") # separa os números de telefone por ";" em uma lista
         cep = contatos_df.loc[i, "Cep"]
         opcoes_texto = {
-    "Notas Novas": 
-        f"Recentemente recebemos o cancelamento de serviços da Vivo. Por isso, precisamos agendar a coleta dos equipamentos Vivo que você não utiliza mais. Para isso, precisamos que você confirme algumas informações:\n\n •  *{cliente}*\n •  *{endereco}, {numero} -{ complemento}*\n •  *CEP: {cep}*\n\nA retirada sempre acontece em horário comercial. Deixamos marcado a retirada dos seus equipamentos para *{data}* (desde que seja feita a confirmação).\n\nSomente maiores de 18 anos poderão realizar esse procedimento, ok? Então caso não tenha ninguém para fazer a entrega dos equipamentos para a nossa equipe, pedimos que você nos responda com a melhor data/horário.\n\nPara sua segurança, você pode confirmar se realmente é a nossa equipe que irá fazer a retirada, conferindo o nº da sua coleta: *{os}*.\n\n*Importante:* Como nossa equipe não está autorizada a entrar na sua residência, pedimos que você já deixe os equipamentos e acessórios (ex: fonte, controle e cabos) em uma sacola ou em uma caixa.\n\nO serviço de coleta é totalmente gratuito e é realizado em parceria com LOCALTEC, que é nosso fornecedor autorizado.\n\nSe quiser mais informações, acesse: www.vivo.com.br/devolverequipamento \nSegue telefone para contato: (11) 3949-7557. \nAté breve,\n\nEquipe Vivo",
+            "Notas Novas": 
+        f"Recentemente recebemos o cancelamento de serviços da Vivo. Por isso, precisamos agendar a coleta dos equipamentos Vivo que você não utiliza mais. Para isso, precisamos que você confirme algumas informações:\n\n •  *{cliente}*\n •  *{endereco}, {numero} -{ complemento}.*\n •  *CEP: {cep}*\n\nA retirada sempre acontece em horário comercial. Deixamos marcado a retirada dos seus equipamentos para *{data}* (desde que seja feita a confirmação).\n\nSomente maiores de 18 anos poderão realizar esse procedimento, ok? Então caso não tenha ninguém para fazer a entrega dos equipamentos para a nossa equipe, pedimos que você nos responda com a melhor data/horário.\n\nPara sua segurança, você pode confirmar se realmente é a nossa equipe que irá fazer a retirada, conferindo o nº da sua coleta: *{os}*.\n\n*Importante:* Como nossa equipe não está autorizada a entrar na sua residência, pedimos que você já deixe os equipamentos e acessórios (ex: fonte, controle e cabos) em uma sacola ou em uma caixa.\n\nO serviço de coleta é totalmente gratuito e é realizado em parceria com LOCALTEC, que é nosso fornecedor autorizado.\n\nSe quiser mais informações, acesse: www.vivo.com.br/devolverequipamento \nSegue telefone para contato: (11) 3949-7557. \nAté breve,\n\nEquipe Vivo",
     "Cliente AUSENTE/MUDOU-SE": 
         f"Olá *{cliente}*\nOS: *{os}*\nEndereço: *{endereco}, {numero} /{ complemento}*\nNosso técnico foi ao local para a coleta dos equipamentos da Vivo e em laudo constou *{ocorrencia}* seria possível Agendar a coleta para dia *{data}*? Caso preferir podemos alterar o endereço da coleta!\n(Informe CEP, NÚMERO DO LOCAL, E COMPLEMENTOS)\n\nEstamos a disposição!\n\nAgradecemos a atenção, aguardamos um retorno",
     "Agendamento LOCALTEC": 
-        f"Olá:*{cliente}*\nOS:*{os}*\nRecebemos um agendamento para retirada dos aparelhos da VIVO para o dia *{data}*, podemos confirmar?\nCaso seja apartamento, pode deixar na portaria se preferir.\nMe confirme o endereço por favor:*{endereco}, {numero} / {complemento}*\n*CEP:{cep}*\nPode ser retirado em horário comercial das 8h as 18h?\nAtenciosamente,\nLocaltec."}
-        opcao = combo_texto.get()  
-        texto = urllib.parse.quote(opcoes_texto[opcao]) 
+        f"Olá:*{cliente}*\nOS:*{os}*\nRecebemos um agendamento para retirada dos aparelhos da VIVO para o dia *{data}*, podemos confirmar?\nCaso seja apartamento, pode deixar na portaria se preferir.\nMe confirme o endereço por favor:*{endereco}, {numero} / {complemento}*\n*CEP:{cep}*\nPode ser retirado em horário comercial das 8h as 18h?\nAtenciosamente,\nLocaltec."
+        }
+        opcao = combo_texto.get()
+        texto = urllib.parse.quote(opcoes_texto[opcao])
         numeros_enviados = []
-        for telefone in telefones: 
-            if telefone.strip() and telefone not in numeros_enviados:
-                enviar_mensagem(telefone, texto)
-                numeros_enviados.append(telefone)
+        for telefone in telefones:
+            for telefone in telefones: 
+                telefone_sem_virgula = telefone.split('.')[0]
+                if isinstance(telefone, str) and telefone.strip() and telefone not in numeros_enviados:       
+                    enviar_mensagem(telefone_sem_virgula, texto)
+                    numeros_enviados.append(telefone_sem_virgula)
+            else:
+                print(f"Telefone inválido: {telefone} OS:{os}")
 
 
 janela = ThemedTk(theme='arc')
@@ -81,15 +101,6 @@ janela.configure(width='400px', height='300px', background='#fff')
 frame_central = tk.Frame(janela, bg='#fff')
 frame_central.pack(expand=True)
 
-def abrir_arquivo():
-    global contatos_df
-    arquivo = filedialog.askopenfilename(filetypes=[("Arquivo Excel", "*.xlsx")])
-    if arquivo:
-        contatos_df = pd.read_excel(arquivo)
-        nome_arquivo = arquivo.split("/")[-1]  # obtém o nome do arquivo a partir do caminho completo
-        rotulo_arquivo.configure(text=f"Arquivo selecionado: {nome_arquivo}")
-
-# adiciona um botão para selecionar o arquivo
 
 
 opcoes = ["Notas Novas", 
